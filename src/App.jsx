aac12aa4d746bf7b53a6a3a0b7d053cbf30ca5d5
SneauxGirl <heatherh@liyudev.com>
2026-02-08 21:56:05 -0800

    demo: form

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,63 @@
 import './App.css'
-import React from 'react';
+import { useState } from 'react';
 
 
 function App() {
+  // State to hold form values
+  const [formData, setFormdata] = useState({
+    name: '',
+    email: '',
+  });
 
-  const items = [
-        { id: 1, name: 'Apple' },
-        { id: 2, name: 'Banana' },
-        { id: 3, name: 'Cherry' },
-        { id: 4, name: 'Date' },
-        { id: 5, name: 'Elderberry' },
-  ];
+  // Handler for input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+      setFormdata({
+        ...formData,
+        [name]: value,
+      });
+    }
+
+  // Handler for form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Above: prevents native page reload
+    // Below - send form to server or whatever
+    console.log('Form submitted:', formData);
+  };
 
   return (
     <div className='container'>
-      <h2>Fruit List</h2>
-      <ui>
-        {
-          items.map((item, index) =>
-             <li key={item.id}>{index + 1}. {item.name}</li>
-            //  Can also use index as key for unique identifier every time
-            // Be careful using OL versus numbering within - styling gets complicated
-          )
-        }
-      </ui>
+      <h2>Simple Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            autoComplete="name"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            autoComplete="email"
+            />
+          </label>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
 
-export default App
+export default App;
