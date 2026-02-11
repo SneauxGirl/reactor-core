9c80e83c2e4c6a6d03ba04a6fe5ee98bc499c671
SneauxGirl <heatherh@liyudev.com>
2026-02-11 04:26:08 -0800

    feat: add Todo, display when signed in

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
 import './App.css';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID, 
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig, {
-  automaticDataCollectionEnabled: false,
-});
-const db = getFirestore(app);
-const auth = getAuth(app);
+import { db, auth, app } from './lib/firebase';
+import Todo from './components/Todo';
 
 function App() {
   const [name, setName] = useState();
@@ -97,6 +81,7 @@ const logOut = () => {
       {
         user && (
           <div>
+            <Todo />
             <p>Logged in as: {user.email}</p>
             <button onClick={logOut}>Sign Out</button>
           </div>
---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID, 
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig, {
+  automaticDataCollectionEnabled: false,
+});
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, app, auth }
